a59bb04b190e97d66404b67869e215420e49d443
web620 <web@goelpower.in>
2026-04-29 15:43:48 +0530

    changes in server file

--- backend/api/server.js
+++ backend/api/server.js
@@ -53,9 +53,10 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 process.on("unhandledRejection", (error) => {
   console.error("Error: ", error.message);
   process.exit(1);
 });
+
+export default app;
